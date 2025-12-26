373eac0c27e080b17986234f67c8d59cbb3538b1
Moritz Jasper <mjasper95@gmail.com>
2025-12-27 00:52:16 +0100

    android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.moritzj.musciapps.simplemetronome.app",
-  appName: "Simple Metronome",
+  appName: "Complete Metronome",
   webDir: "dist",
   bundledWebRuntime: false
 };
